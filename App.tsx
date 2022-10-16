2fe082cb0c803450c08893a3d1fabd0c735c88e2
denruss161 <108678500+denruss161@users.noreply.github.com>
2022-10-17 05:06:04 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Jocker 2022</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-71tj9b
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/denruss161/react-ts-71tj9b)
